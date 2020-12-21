f5606d6fed7bb9165564f2a689c762e7ec30eb71
Alex <alex.s.miao@gmail.com>
2020-12-20 21:00:26 -0500

    Add scalar polynomials

---
+++ curve25519/scalar_polynomial.go
@@ -0,0 +1,25 @@
+package curve25519
+
+// Polynomial is a represtation of a polynomial over Scalars
+type Polynomial struct {
+	Coefficients []Scalar
+}
+
+// Degree gets the degree of the polynomial
+func (p *Polynomial) Degree() int {
+	i := len(p.Coefficients) - 1
+	for ; i > 0 && IsEqualScalar(p.Coefficients[i], ScalarZero); i-- {
+	}
+	return i
+}
+
+// Evaluate evaluates the polynomial p at a point x
+func (p *Polynomial) Evaluate(x Scalar) Scalar {
+	degree := p.Degree()
+	evaluation := p.Coefficients[degree]
+	for i := degree - 1; i >= 0; i-- {
+		evaluation = MultScalar(evaluation, x)
+		evaluation = AddScalar(evaluation, p.Coefficients[i])
+	}
+	return evaluation
+}
