7b63aeb445a2d01070852fba8698515f593076b1
Alex <alex.s.miao@gmail.com>
2020-12-28 00:10:49 -0500

    Add Pedersen VSS

--- pedersen/pedersen.go
+++ pedersen/pedersen.go
@@ -23,6 +23,14 @@ type Commitment curve25519.Point
 // Decommitment is the random value r used
 type Decommitment curve25519.Scalar
 
+// Share is composed of values used in Pedersen VSS to reconstruct a secret
+type Share struct {
+	Index       int
+	IndexScalar curve25519.Scalar
+	S           curve25519.Scalar
+	R           curve25519.Scalar
+}
+
 // GenerateParams picks two random group elements for generating commitments
 func GenerateParams() *Params {
 	g := curve25519.RandomPoint()
@@ -83,4 +91,150 @@ func VerifyCommitment(params *Params, commitment *Commitment, m Message, r *Deco
 	}
 
 	return curve25519.IsEqualPoint(curve25519.Point(*commitment), c), nil
+}
+
+// VSSShare performs the intial dealer's step for a Pedersen VSS on the message m
+// for t-of-n reconstruction.
+func VSSShare(params *Params, m Message, t int, n int) (*[]Share, *[]Commitment, error) {
+	if t < 1 || n < 1 || t > n {
+		return nil, nil, fmt.Errorf("invalid share generation parameters")
+	}
+
+	// The shares to be distributed to participants
+	var shares []Share
+
+	// The commitments to the coefficient of polynomials used to verify
+	// correctness of shares
+	var verifications []Commitment
+
+	// Construct shares to be distributed to participants individually
+	var coeffF = make([]curve25519.Scalar, t)
+	var coeffG = make([]curve25519.Scalar, t)
+
+	// Get commitment to the secret
+	commitment, decommitment, err := GenerateCommitment(params, m)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error generating message commitment")
+	}
+	coeffF[0] = curve25519.Scalar(m)
+	coeffG[0] = curve25519.Scalar(*decommitment)
+
+	verifications = append(verifications, *commitment)
+
+	// Generate random values for remaining coefficients
+	for i := 1; i < t; i++ {
+		coeffF[i] = curve25519.RandomScalar()
+		// Get commitment the random polynomial coefficients
+		commitment, decommitment, err = GenerateCommitment(params, Message(coeffF[i]))
+		if err != nil {
+			return nil, nil, fmt.Errorf("error generating commitment")
+		}
+		coeffG[i] = curve25519.Scalar(*decommitment)
+
+		// verification of ith coefficient is g^a_i * h^b_i
+		verifications = append(verifications, *commitment)
+	}
+
+	f := curve25519.Polynomial{
+		Coefficients: coeffF,
+	} // f(x) = a_0 + a_1 * x + a_2 * x^2 + ... + a_t * x^t where a_0 = m and a_1,...,a_t are random
+	g := curve25519.Polynomial{
+		Coefficients: coeffG,
+	} // g(x) = b_0 + b_1 * x + b_2 * x^2 + ... + b_t * x^t where b_0,...,b_t are random
+
+	// Perform Shamir secret sharing on the generated polynomials to construct shares
+	evalPoint := curve25519.ScalarZero
+	for i := 1; i <= n; i++ {
+		evalPoint = curve25519.AddScalar(evalPoint, curve25519.ScalarOne)
+
+		shares = append(shares, Share{
+			Index:       i,
+			IndexScalar: evalPoint,
+			S:           f.Evaluate(evalPoint),
+			R:           g.Evaluate(evalPoint),
+		}) // The share of participant i is (s_i, r_i) = (f(i), g(i))
+	}
+
+	return &shares, &verifications, nil
+}
+
+// VSSVerify performs verification of a received share with the broadcasted
+// verification.
+func VSSVerify(params *Params, share Share, verifications []Commitment) (bool, error) {
+	t := len(verifications)
+	gs, err := curve25519.MultPointScalar(params.G, share.S)
+	if err != nil {
+		return false, fmt.Errorf("error in evaluating share")
+	}
+
+	hr, err := curve25519.MultPointScalar(params.H, share.R)
+	if err != nil {
+		return false, fmt.Errorf("error in evaluating share")
+	}
+
+	shareResult, err := curve25519.AddPoint(gs, hr) // Compute g^{s_i} * h^{r_i}
+	if err != nil {
+		return false, fmt.Errorf("error in evaluating share")
+	}
+
+	// Compute \prod_{j=0}^{t-1}E_j^{i^j} where E_j is the jth entry in the verification vector
+	iMult := share.IndexScalar
+	verificationResult := curve25519.Point(verifications[0])
+	for j := 1; j < t; j++ {
+		ejij, err := curve25519.MultPointScalar(curve25519.Point(verifications[j]), iMult)
+		if err != nil {
+			return false, fmt.Errorf("error in evaluating verification")
+		}
+		iMult = curve25519.MultScalar(iMult, share.IndexScalar)
+		verificationResult, err = curve25519.AddPoint(verificationResult, ejij)
+		if err != nil {
+			return false, fmt.Errorf("error in evaluating verification")
+		}
+	}
+
+	// Check if g^{s_i} * h^{r_i} = \prod_{j=0}^{t-1}E_j^{i^j}
+	return curve25519.IsEqualPoint(shareResult, verificationResult), nil
+}
+
+// VSSReconstruct reconstructs the secret shared in the Pedersen VSS scheme
+func VSSReconstruct(params *Params, shares []Share, verifications []Commitment) (*Message, error) {
+	t := len(verifications)
+	n := len(shares)
+	var validShares []Share
+
+	// Find t valid shares to determine what points to interpolate through
+	for i := 0; i < n && i-len(validShares) <= n-t && len(validShares) < t; i++ {
+		isValid, err := VSSVerify(params, shares[i], verifications)
+		if err != nil {
+			return nil, fmt.Errorf("error in share verification")
+		}
+
+		if isValid {
+			validShares = append(validShares, shares[i])
+		}
+	}
+
+	if len(validShares) < t {
+		return nil, fmt.Errorf("insufficient valid shares") // Unable to reconstruct due to insufficient valid shares
+	}
+
+	// Polynomial interpolation evaluated at 0
+	var sum curve25519.Scalar
+	for i := 0; i < t; i++ {
+
+		term := validShares[i].S
+		for j := 0; j < t; j++ {
+			if i != j {
+				denom, err := curve25519.InvertScalar(curve25519.SubScalar(validShares[j].IndexScalar, validShares[i].IndexScalar))
+				if err != nil {
+					return nil, fmt.Errorf("error in polynomial interpolation")
+				}
+				term = curve25519.MultScalar(term, curve25519.MultScalar(validShares[j].IndexScalar, denom))
+			}
+		}
+		sum = curve25519.AddScalar(sum, term)
+	}
+
+	m := Message(sum)
+	return &m, nil
 }
--- pedersen/pedersen_test.go
+++ pedersen/pedersen_test.go
@@ -38,3 +38,58 @@ func TestPedersenCommitment(t *testing.T) {
 	}
 	assert.False(t, isValid, "Verification fails for wrong decommitment")
 }
+
+func TestPedersenVSS(t *testing.T) {
+	m := Message(curve25519.RandomScalar())
+
+	params := GenerateParams()
+
+	shares, verifications, err := VSSShare(params, m, 3, 4)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	isValid1, err := VSSVerify(params, (*shares)[0], *verifications)
+	if err != nil {
+		log.Fatal(err)
+	}
+	assert.True(t, isValid1, "Verification is verifies valid share")
+
+	isValid2, err := VSSVerify(params, (*shares)[1], *verifications)
+	if err != nil {
+		log.Fatal(err)
+	}
+	assert.True(t, isValid2, "Verification is verifies valid share")
+
+	isValid3, err := VSSVerify(params, (*shares)[2], *verifications)
+	if err != nil {
+		log.Fatal(err)
+	}
+	assert.True(t, isValid3, "Verification is verifies valid share")
+
+	reconstruction1, err := VSSReconstruct(params, *shares, *verifications)
+	if err != nil {
+		log.Fatal(err)
+	}
+	assert.Equal(t, m, *reconstruction1, "VSS is consistent")
+
+	invalidShare := Share{
+		Index:       1,
+		IndexScalar: curve25519.ScalarOne,
+		S:           curve25519.RandomScalar(),
+		R:           curve25519.RandomScalar(),
+	}
+	modifiedShares := *shares
+	modifiedShares[0] = invalidShare
+
+	reconstruction2, err := VSSReconstruct(params, modifiedShares, *verifications)
+	if err != nil {
+		log.Fatal(err)
+	}
+	assert.Equal(t, m, *reconstruction2, "VSS with modified shares, above threshold")
+
+	modifiedShares[2] = invalidShare
+
+	_, err = VSSReconstruct(params, modifiedShares, *verifications)
+	assert.EqualError(t, err, "insufficient valid shares", "VSS fails below threshold valid shares")
+}
