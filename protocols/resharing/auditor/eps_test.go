c9e51b42036593dc6a4ab5a48f053e93c9af13d2
Fabrice Benhamouda <fabrice.benhamouda@normalesup.org>
2021-08-15 20:21:54 -0400

    implement future broadcast in dealing - not in other steps

---
+++ protocols/resharing/auditor/eps_test.go
@@ -0,0 +1,63 @@
+package auditor
+
+import (
+	"fmt"
+	"github.com/shaih/go-yosovss/primitives/curve25519"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestReconstructEpsKey(t *testing.T) {
+	testCases := []struct {
+		n int
+		d int
+	}{
+		{11,5},
+		{2,1},
+		{21,3},
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("n=%d,d=%d", tc.n, tc.d), func(t *testing.T) {
+			require := require.New(t)
+			assert := assert.New(t)
+
+			n := tc.n
+			d := tc.d
+
+			epsKey, epsL, hashEps, err := GenerateAllEps(n, d)
+			require.NoError(err)
+
+			for k := 0; k < n; k++ {
+				shares := make([]*curve25519.Scalar, n)
+				for l := 0; l < n; l++ {
+					shares[l] = &epsL[l].Eps[k]
+				}
+
+				// Check reconstruction with all shares
+				reconsKey, err := ReconstructEpsKey(n, d, shares, hashEps[k])
+				require.NoError(err)
+				assert.Equal(epsKey[k], reconsKey, "reconstructed key match original key when reconstruction with all shares")
+
+				// Check reconstruction with the last d+1 shares only
+				for l := 0; l < n-(d+1); l++ {
+					shares[l] = nil
+				}
+				reconsKey, err = ReconstructEpsKey(n, d, shares, hashEps[k])
+				require.NoError(err)
+				assert.Equal(epsKey[k], reconsKey, "reconstructed key match original key when reconstruction with d+1 shares")
+
+				// Check reconstruction with some invalid shares
+				for l := 0; l < n-(d+1); l++ {
+					s := curve25519.GetScalar(42) // 42 is not the answer here :-) It's an invalid share
+					shares[l] = &s
+				}
+				reconsKey, err = ReconstructEpsKey(n, d, shares, hashEps[k])
+				require.NoError(err)
+				assert.Equal(epsKey[k], reconsKey, "reconstructed key match original key when reconstruction with d+1 shares")
+			}
+		})
+	}
+
+}
--- protocols/resharing/auditor/step1_dealing.go
+++ protocols/resharing/auditor/step1_dealing.go
@@ -15,14 +15,29 @@ type DealingMessage struct {
 	// (com[j][0])_j=0,...,n-1 are commitments to first-level shares sigma_{j+1} of the secret s of the dealer
 	// and for each j, com[j][k] for k=1,...,n are commitments to the shares sigma_{j+1,k} of the share sigma_{j+1}
 	EncVerM []curve25519.Ciphertext `codec:"V"` // EncVerM[k] is an encryption under the verification
-	// key committee of message M[k] (type VerificationMK)
-	// TODO future broadcast
+	// committee member k's key of message M[k] (type VerificationMK)
+	EncResM []curve25519.SymmetricCiphertext `codec:"R"` // EncResM[k] is a symmetric encryption of M[k]
+	// under a fresh symmetric key K generated as follows:
+	// generate a random scalar eps_{k+1} that is secret-shared into eps_{k+1,1},...,eps_{k+1,n}
+	// K = HKDF(eps_k+1)
+	// k in 0,...,n-1
+	EncEpsL []curve25519.Ciphertext `codec:"e"` // EncEpsL[l] is an encryption under the resolution
+	// committee member l's key of message EpsL described below
+	// l in 0,...,n-1
+	HashEps [][][HashLength]byte `codec:"h"` // HashEps[k][l] is the hash of eps_{k,l+1}
+	// k,l in 0,...,n-1
 }
 
 // VerificationMK is the message M[k] for verification committee member k
 type VerificationMK struct {
 	S []curve25519.Scalar // sigma_1k,..., sigma_nk
 	R []curve25519.Scalar // rho_1k, ..., rho_nk
+}
+
+// EpsL is the message for resolution committee member l
+type EpsL struct {
+	Eps []curve25519.Scalar // eps_{1,l+1},...,eps_{n,l+1}
+	// TODO: not optimized as we could use a smaller modulus, but that's good enough for this implementation
 }
 
 func GenerateDealerSharesCommitments(
@@ -57,6 +72,8 @@ func GenerateDealerSharesCommitments(
 func PerformDealing(pub *PublicInput, prv *PrivateInput) (*DealingMessage, error) {
 	msg := &DealingMessage{
 		EncVerM: make([]curve25519.Ciphertext, pub.N),
+		EncResM: make([]curve25519.SymmetricCiphertext, pub.N),
+		EncEpsL: make([]curve25519.Ciphertext, pub.N),
 	}
 
 	shares, comS, err := GenerateDealerSharesCommitments(&pub.VSSParams, &prv.Share.S, &prv.Share.R)
@@ -69,18 +86,48 @@ func PerformDealing(pub *PublicInput, prv *PrivateInput) (*DealingMessage, error
 		return nil, fmt.Errorf("invalid committe length")
 	}
 
-	// Encryption for verification committee
-	for k, verId := range pub.Committees.Ver {
-		mk := VerificationMK{
-			S: make([]curve25519.Scalar, pub.N),
-			R: make([]curve25519.Scalar, pub.N),
-		}
-		for j := 0; j < pub.N; j++ {
-			mk.S[j] = shares[j][k].S
-			mk.R[j] = shares[j][k].R
-		}
-		msg.EncVerM[k], err = curve25519.Encrypt(pub.EncPKs[verId], msgpack.Encode(mk))
+	// Generate keys and shares for resolution committee (future broadcast)
+	var epsL []EpsL
+	var epsKeys []curve25519.Key
+	epsKeys, epsL, msg.HashEps, err = GenerateAllEps(pub.N, pub.T)
+	if err != nil {
+		return nil, err
+	}
+
+	// Encryption for verification committee and resolution committee
+	for k := 0; k < pub.N; k++ {
+		// Compute M[K]
+		mk := ComputeMK(pub.N, shares, k)
+
+		mkMsg := msgpack.Encode(mk)
+
+		// Encrypt M[k] for the verification committee
+		msg.EncVerM[k], err = curve25519.Encrypt(pub.EncPKs[pub.Committees.Ver[k]], mkMsg)
+
+		// Encrypt M[k] for resolution / future broadcast
+		// Use a zero nonce as the key is fresh
+		zeroNonce := curve25519.Nonce{}
+		msg.EncResM[k], err = curve25519.SymmetricEncrypt(epsKeys[k], zeroNonce, mkMsg)
+	}
+
+	// Encrypt epsL for each resolution committee member
+	for l := 0; l < pub.N; l++ {
+		msg.EncEpsL[l], err = curve25519.Encrypt(pub.EncPKs[pub.Committees.Res[l]], msgpack.Encode(epsL[l]))
 	}
 
 	return msg, nil
+}
+
+
+// ComputeMK computes the verification committee message M[k] see above
+func ComputeMK(n int, shares [][]vss.Share, k int) VerificationMK {
+	mk := VerificationMK{
+		S: make([]curve25519.Scalar, n),
+		R: make([]curve25519.Scalar, n),
+	}
+	for j := 0; j < n; j++ {
+		mk.S[j] = shares[j][k].S
+		mk.R[j] = shares[j][k].R
+	}
+	return mk
 }
