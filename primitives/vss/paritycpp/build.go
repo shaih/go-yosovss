d1d00a7f35c86e14908a2b0427c8f8e414f9c2a8
Shai Halevi <shaih@alum.mit.edu>
2021-12-26 04:29:16 -0500

    Adding flags '-lgmp -lm' to compile NTL/GMP on Ubuntu

--- primitives/vss/paritycpp/build.go
+++ primitives/vss/paritycpp/build.go
@@ -3,5 +3,5 @@ package paritycpp
 // TODO FIXME: Hardcoding /usr/local/include is abit dirty
 
 // #cgo CXXFLAGS: -std=gnu++17 -I/usr/local/include
-// #cgo LDFLAGS: -L/usr/local/lib -lntl
+// #cgo LDFLAGS: -L/usr/local/lib -lntl -lgmp -lm
 import "C"
